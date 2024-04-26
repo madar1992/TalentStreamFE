fc185f426e06100c64ee4133c4ef48f57eeb678e
madar1992 <96854154+madar1992@users.noreply.github.com>
2024-04-26 22:38:08 +0530

    Update ApplicantAPIService.js

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 //export const apiUrl = 'http://localhost:8081';
 
-export const apiUrl = 'https://kqryamxpv3.ap-south-1.awsapprunner.com';
+export const apiUrl = 'https://talentstreambe.onrender.com';
 
 
 
